d21fcdb90582e58a587a3832ad4ae002cf26b022
Mario-Davalos <a25110281@ceti.mx>
2026-05-17 17:39:56 +0000

    practica 4

---
+++ include/seriefocos.hpp
@@ -0,0 +1,33 @@
+#pragma once 
+#include <iostream>
+#include <foco.hpp>
+
+using namespace std;
+class serieFocos
+
+private:
+foco focos[10];
+public: 
+serieFocos(){}
+˜seriefocos(){}
+void mostrarFocos()
+{
+    for(auto$$f : focos)
+    {
+        cout <<f.leerEstado()<<endl;
+    }
+
+    }
+    void EncenderFocos(){
+        for (auto &&f:focos)
+
+    {
+    f.encender();
+    }
+}
+void apagarfocos(){
+for(auto  &&f:focos)
+{
+    f.apagar();
+}
+};
